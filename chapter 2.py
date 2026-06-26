0dd8a193f3c9e443dade85759d00e6783572c45b
Harsh Vardhan <kharshvardhan007@gmail.com>
2026-06-27 01:02:32 +0530

    Add files via upload

---
+++ chapter 2.py
@@ -0,0 +1,12 @@
+#str1 = "this is a string.\t we are creating it in python."
+##print(str1)
+
+str1 = "harsh"
+len1 = len(str1)
+print(len1)
+
+str2 = "vardhan"
+len2 = len(str2)
+print(len2)
+
+print(str1+str2)
---
+++ question.py
@@ -0,0 +1,9 @@
+#a = float(input("enter first  :"))
+#b = float(input("enter second  :"))
+
+
+#print("avg=",(a+b)/2)
+a = int(input("enter first :"))
+b = int(input("enter second :"))
+
+print(a>=b)
