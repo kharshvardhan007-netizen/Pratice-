1e6081f45453a45a6a75f4265e833bc0cd68aa10
Harsh Vardhan <kharshvardhan007@gmail.com>
2026-06-26 23:58:15 +0530

    Add files via upload

---
+++ check even or odd.cpp
@@ -0,0 +1,18 @@
+//to check number is negative or positive 
+#include<stdio.h>
+int main ()
+{
+	int n;
+	printf("print any number : ");
+	scanf("%d",&n);
+	if(n%2==0)
+	{
+        printf("number is even\n");
+    }
+    else if (n%2!=0)
+    {
+		printf("number is odd\n");
+	}	
+}
+
+	
