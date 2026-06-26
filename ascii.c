9017002eeb3896883cc4a1cfd1b7a66f6b8d5bf2
Harsh Vardhan <kharshvardhan007@gmail.com>
2026-06-26 23:43:31 +0530

    Add comparison program for two numbers

---
+++ ascii.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main ()
+{
+	int a,b;
+	printf ("enter first number (a):");
+	scanf ("%d",&a);
+  printf("enter the second number(b):");
+  scanf ("%d",&b);
+	if (a>b)
+	printf ("a is grater\n");
+	else 
+	printf ("b is grater\n");
+	return 0;
+	
+	
+}
+
+	
