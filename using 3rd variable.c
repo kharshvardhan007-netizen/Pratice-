ecef30fb0b1bdfe5cde362d1b5204d521e277393
Harsh Vardhan <kharshvardhan007@gmail.com>
2026-06-27 01:01:18 +0530

    Correct addition logic and function structure
    Fix syntax errors and improve the addition function.

---
+++ using 3rd variable.c
@@ -0,0 +1,17 @@
+// functions:no args and no return value
+#include<stdio.h>
+void add();
+void main()
+{
+   float a,b,sum;
+   printf("enter the value of a and b");
+   scaanf("%f%f",&a,&b);
+   sum=a+b;
+   printf("sum=%f",sum);
+{
+void main();
+{
+     add();
+ }
+}
+}
