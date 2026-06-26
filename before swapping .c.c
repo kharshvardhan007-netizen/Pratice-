076ecefbb9e0349c9cef2788635d33e2a09824a2
Harsh Vardhan <kharshvardhan007@gmail.com>
2026-06-26 23:56:19 +0530

    Add files via upload

---
+++ before swapping .c.c
@@ -0,0 +1,12 @@
+//swapping using 3rd variable 
+#include<stdio.h>
+
+void main()
+{
+	int a,b,c;
+	printf("enter the value of a and b");
+	scanf("%d%d,&a,&b");
+	
+	printf("before swapping value of a=%d/n",a);
+	printf("befire swapping value of b=%d/n",b) ;
+}
