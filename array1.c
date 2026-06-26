6837d99b83535953cad73bcb1a15cd3f612d961a
Harsh Vardhan <kharshvardhan007@gmail.com>
2026-06-26 23:49:56 +0530

    Create array1.c

---
+++ array1.c
@@ -0,0 +1,24 @@
+// array input and output
+//swapping using call by value 
+#include<stdio.h>
+void main ()
+
+{
+	int a [5]={10,20,30,40,50},i;
+	//display: manual
+	printf("first element = %d\n",a[0]);
+	printf("second element = %d\n",a[1]);
+	printf("third element = %d\n",a[2]);
+	printf("fourth element = %d\n",a[3]);
+	printf("fifth element = %d\n",a[4]);
+	//dispaly using loop
+	printf("dispaly the elementof array\n");
+	for(i=0;i<5;i++)
+	{
+	printf("%d\t",a[i]);
+	}
+	
+}
+
+
+
