b22cb2f9f63ea2ec8340543a51bbd0cf7af8abfd
Harsh Vardhan <kharshvardhan007@gmail.com>
2026-06-27 00:15:55 +0530

    Create fun5.c

---
+++ fun5.c
@@ -0,0 +1,23 @@
+// functions:with args and with  no.return value
+#include<stdio.h>
+float add();
+float add()
+{
+	float i,n,sum;
+	for(i=1;i<n;i++)
+	{
+		if(i==0)
+		sum=sum+i;
+	}
+	if (sum==n)
+	printf("no is perfect");
+	else
+	   printf("no is not perfect");
+}
+void main()
+{
+	int n;
+	printf("enter any no");
+	scanf("%d",&n);
+	
+}
