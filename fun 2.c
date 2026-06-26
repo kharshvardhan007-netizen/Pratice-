db4719d66ac688e11d005f2b22738c7ee21f2e11
Harsh Vardhan <kharshvardhan007@gmail.com>
2026-06-27 00:03:50 +0530

    Add files via upload

---
+++ fun 2.c
@@ -0,0 +1,20 @@
+// functions:no args and no return value
+#include<stdio.h>
+void add(float,float);
+void add(float p,float q)
+{
+	float
+	sum=p+q;
+	printf("sum=%d",sum);
+}
+void main()
+{
+	
+   float p,q,sum;
+   printf("enter the value of a and b");
+   scanf("%f%f",&p,&q);
+   add(p,q);//actual parametre
+   printf("sum=%f",sum);
+}
+
+
