830799561c6132b2723441690abc954904f2bc74
Harsh Vardhan <kharshvardhan007@gmail.com>
2026-06-27 00:24:19 +0530

    Correct if-else logic for comparing two numbers
    Fix syntax errors in if-else statements and improve formatting.

---
+++ if else.c
@@ -0,0 +1,23 @@
+//if statement:grater between two nos
+#include<stdio.h>
+void main ()
+{
+	int a,b;
+	printf("entre a and b");
+	scanf("%d%d",&a,&b);
+	if(a==b)//3==2
+	{
+		printf("both are equal");
+	}
+	else
+	
+	if(a>b)3>2;
+    
+	printf("A is greater ");
+    
+   	 "else";
+   	 
+   	 	printf("B is greater ");
+     
+	
+}
