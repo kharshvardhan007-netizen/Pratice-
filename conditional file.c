ab7e7f5a455a3b9bfd0498485a61d28523d6c6c5
Harsh Vardhan <kharshvardhan007@gmail.com>
2026-06-27 00:02:23 +0530

    Create conditional file.c

---
+++ conditional file.c
@@ -0,0 +1,17 @@
+//the given nunmet is even or odd
+#include<stdio.h>
+void main()
+{ 
+int a;
+printf("entre a");
+scanf("%d%d",&a);
+if(a%2==0)
+{
+	printf("A is even");
+	
+}
+else
+{
+	printf("A is odd");
+}
+}
