fd8ce4f5bfa2233e3be0967fb2601d4be4bb3a6d
Harsh Vardhan <kharshvardhan007@gmail.com>
2026-06-27 00:46:08 +0530

    Add files via upload

---
+++ number is + - 0.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main ()
+{
+	int num;
+	printf ("print any number");
+	scanf ("%d",&num);
+	if ("n>0")
+	{
+       printf("number is positive\n");
+	}
+	else if ("n<o")
+	{
+		printf("number is negative\n");
+	}
+	else
+	{
+		printf("number is zero\n");
+	}
+	
+	
+}
+
+	
